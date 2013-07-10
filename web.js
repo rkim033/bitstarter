3351bcafe5a0533203d42acc84770ab0da3fa1c7
Ryan <rkim033@gmail.com>
2013-07-10 06:20:11 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/index.html' function (err, data) {
+  response.send(fs.readFileSync('index.html' function (err, data) {
   if (err) throw err;
   console.log(data);
 }));
